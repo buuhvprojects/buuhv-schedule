8552c6b7b36f0d3be679f67b9e659d1e87be89ec
Bruno Nascimento <robertbrunotrabalho@gmail.com>
2021-04-08 10:50:33 -0300

    build

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,32 @@
+import ScheduleRegister, { RegisterData } from "./modules/register/controller";
+import ScheduleRemove, { RemoveData } from "./modules/remove/controller";
+import ScheduleUpdate, { UpdateData } from "./modules/update/controller";
+import ScheduleGet, { GetData } from "./modules/get/controller";
+import { configOptions } from "./services/database";
+declare namespace BuuhVSchedule {
+    /**
+     * Novo agendamento
+     */
+    class Register extends ScheduleRegister {
+        constructor(config: configOptions, data: RegisterData);
+    }
+    /**
+     * Remover agendamento
+     */
+    class Remove extends ScheduleRemove {
+        constructor(config: configOptions, data: RemoveData);
+    }
+    /**
+     * Atualizar agendamento
+     */
+    class Update extends ScheduleUpdate {
+        constructor(config: configOptions, data: UpdateData);
+    }
+    /**
+     * Carregar agendamento
+     */
+    class Get extends ScheduleGet {
+        constructor(config: configOptions, data: GetData);
+    }
+}
 export default BuuhVSchedule;
-import BuuhVSchedule from "./dist/src";
--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-import BuuhVSchedule from './dist/src';
-
-export default BuuhVSchedule;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const controller_1 = require("./modules/register/controller");
+const controller_2 = require("./modules/remove/controller");
+const controller_3 = require("./modules/update/controller");
+const controller_4 = require("./modules/get/controller");
+var BuuhVSchedule;
+(function (BuuhVSchedule) {
+    /**
+     * Novo agendamento
+     */
+    class Register extends controller_1.default {
+        constructor(config, data) {
+            super(config, data);
+        }
+    }
+    BuuhVSchedule.Register = Register;
+    /**
+     * Remover agendamento
+     */
+    class Remove extends controller_2.default {
+        constructor(config, data) {
+            super(config, data);
+        }
+    }
+    BuuhVSchedule.Remove = Remove;
+    /**
+     * Atualizar agendamento
+     */
+    class Update extends controller_3.default {
+        constructor(config, data) {
+            super(config, data);
+        }
+    }
+    BuuhVSchedule.Update = Update;
+    /**
+     * Carregar agendamento
+     */
+    class Get extends controller_4.default {
+        constructor(config, data) {
+            super(config, data);
+        }
+    }
+    BuuhVSchedule.Get = Get;
+})(BuuhVSchedule || (BuuhVSchedule = {}));
+exports.default = BuuhVSchedule;
+//# sourceMappingURL=index.js.map
